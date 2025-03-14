fa822fd1962d13bc13557bdfcc79be93c3a54473
Mashqur <mashqur.himel@gmail.com>
2025-03-15 01:00:15 +0600

    added functionality from clicking item to detail page

--- code/script.js
+++ code/script.js
@@ -3,3 +3,7 @@ function displayImage(imageSrc) {
   const imageDisplayDiv = document.getElementById('image-display');
   imageDisplayDiv.src = imageSrc
 }
+
+function gotoDetailPage() {
+  window.location.href = "itemDetail.html"
+}
